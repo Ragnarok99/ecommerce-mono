ff29b9e0971ac571c57c732edf83af8e3e24a7a4
Sasori Kun <juanloki3@hotmail.com>
2022-07-02 12:15:57 -0500

    feat[BE]: add endpoint delete product by id (#6)

--- apps/server/src/routes/Product.ts
+++ apps/server/src/routes/Product.ts
@@ -6,5 +6,6 @@ const router = express.Router();
 router.get('/:id', ProductController.getById);
 router.get('/', ProductController.getAll);
 router.post('/', ProductController.create);
+router.delete('/:id', ProductController.remove);
 
 export default router;
