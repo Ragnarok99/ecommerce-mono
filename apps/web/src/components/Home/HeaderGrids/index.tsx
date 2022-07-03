2e00a468a64189a5783d3fd8fa4dd92a74e04814
ragnarok <ragnarok@ragnaroks-MacBook-Pro.local>
2022-07-02 19:56:07 -0500

    [chore] fix build for server v4

--- apps/web/src/components/Home/HeaderGrids/index.tsx
+++ apps/web/src/components/Home/HeaderGrids/index.tsx
@@ -1,53 +1,54 @@
-import Image from 'next/image';
+// import Image from 'next/image';
 
-import banner1 from '../../../assets/webp/banner-1.webp';
-import banner2 from '../../../assets/webp/banner-2.webp';
-import banner3 from '../../../assets/webp/banner-3.webp';
-import banner4 from '../../../assets/webp/banner-4.webp';
-import banner5 from '../../../assets/webp/banner-5.webp';
-import banner6 from '../../../assets/webp/banner-6.webp';
+// import banner1 from '../../../assets/webp/banner-1.webp';
+// import banner2 from '../../../assets/webp/banner-2.webp';
+// import banner3 from '../../../assets/webp/banner-3.webp';
+// import banner4 from '../../../assets/webp/banner-4.webp';
+// import banner5 from '../../../assets/webp/banner-5.webp';
+// import banner6 from '../../../assets/webp/banner-6.webp';
 
 const HeaderGrids = () => (
-  <div className="mx-auto mb-12 grid max-w-[1920px] grid-cols-2 gap-2 px-2.5 sm:grid-cols-9 md:mb-14 md:gap-2.5 xl:mb-16">
-    <div className="col-span-full sm:col-span-5">
-      <a className="group relative flex h-full overflow-hidden" href="/">
-        <Image
-          layout="fill"
-          className="w-full bg-gray-300 object-cover"
-          src={banner1}
-        />
-      </a>
-    </div>
-    <div className="col-span-1 mx-auto sm:col-span-2">
-      <a className="group relative flex h-full overflow-hidden" href="/">
-        <Image className="w-full bg-gray-300 object-cover" src={banner2} />
-      </a>
-    </div>
-    <div className="col-span-1 mx-auto sm:col-span-2">
-      <a className="group relative flex h-full overflow-hidden" href="/">
-        <Image className="w-full bg-gray-300 object-cover" src={banner3} />
-      </a>
-    </div>
-    <div className="col-span-1 mx-auto sm:col-span-2">
-      <a className="group relative flex h-full overflow-hidden" href="/">
-        <Image className="w-full bg-gray-300 object-cover" src={banner4} />
-      </a>
-    </div>
-    <div className="col-span-1 mx-auto sm:col-span-2">
-      <a className="group relative flex h-full overflow-hidden" href="/">
-        <Image className="w-full bg-gray-300 object-cover" src={banner5} />
-      </a>
-    </div>
-    <div className="col-span-full sm:col-span-5">
-      <a className="group relative flex h-full overflow-hidden" href="/">
-        <Image
-          layout="fill"
-          className="w-full bg-gray-300 object-cover"
-          src={banner6}
-        />
-      </a>
-    </div>
-  </div>
+  // <div className="mx-auto mb-12 grid max-w-[1920px] grid-cols-2 gap-2 px-2.5 sm:grid-cols-9 md:mb-14 md:gap-2.5 xl:mb-16">
+  //   <div className="col-span-full sm:col-span-5">
+  //     <a className="group relative flex h-full overflow-hidden" href="/">
+  //       <Image
+  //         layout="fill"
+  //         className="w-full bg-gray-300 object-cover"
+  //         src={banner1}
+  //       />
+  //     </a>
+  //   </div>
+  //   <div className="col-span-1 mx-auto sm:col-span-2">
+  //     <a className="group relative flex h-full overflow-hidden" href="/">
+  //       <Image className="w-full bg-gray-300 object-cover" src={banner2} />
+  //     </a>
+  //   </div>
+  //   <div className="col-span-1 mx-auto sm:col-span-2">
+  //     <a className="group relative flex h-full overflow-hidden" href="/">
+  //       <Image className="w-full bg-gray-300 object-cover" src={banner3} />
+  //     </a>
+  //   </div>
+  //   <div className="col-span-1 mx-auto sm:col-span-2">
+  //     <a className="group relative flex h-full overflow-hidden" href="/">
+  //       <Image className="w-full bg-gray-300 object-cover" src={banner4} />
+  //     </a>
+  //   </div>
+  //   <div className="col-span-1 mx-auto sm:col-span-2">
+  //     <a className="group relative flex h-full overflow-hidden" href="/">
+  //       <Image className="w-full bg-gray-300 object-cover" src={banner5} />
+  //     </a>
+  //   </div>
+  //   <div className="col-span-full sm:col-span-5">
+  //     <a className="group relative flex h-full overflow-hidden" href="/">
+  //       <Image
+  //         layout="fill"
+  //         className="w-full bg-gray-300 object-cover"
+  //         src={banner6}
+  //       />
+  //     </a>
+  //   </div>
+  // </div>
+  <div>hola</div>
 );
 
 export default HeaderGrids;
