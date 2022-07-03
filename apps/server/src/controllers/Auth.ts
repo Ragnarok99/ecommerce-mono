e1563f57aacdb80c11f3c4fe6c5472c8cbdc3ef3
Sasori Kun <juanloki3@hotmail.com>
2022-07-03 14:38:03 -0500

    feat[BE]/encrypted-password

--- apps/server/src/controllers/Auth.ts
+++ apps/server/src/controllers/Auth.ts
@@ -1,18 +1,25 @@
 import { Request, Response } from 'express';
+import * as bcrypt from 'bcrypt';
 import User from '../models/User';
+
+const { SALT_NUMBER = 20 } = process.env;
 
 const signup = async (req: Request, res: Response) => {
   try {
     const { body } = req;
+    const salt = await bcrypt.genSalt(Number(SALT_NUMBER));
+    const encryptedPassword = await bcrypt.hash(body.password, salt);
     const user = new User({
       ...body,
+      password: encryptedPassword,
+      salt,
       updated_at: new Date(),
       created_at: new Date(),
     });
 
     await user.save();
 
-    res.status(201).send(user);
+    res.status(201).send(user.toJSON());
   } catch (errors) {
     res.status(500).send({ message: errors });
   }
--- apps/server/src/schemas/User.ts
+++ apps/server/src/schemas/User.ts
@@ -6,9 +6,18 @@ const UserSchema = new Schema({
   username: { type: String, required: true },
   // TODO: validate emails address
   email: { type: String, required: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true, select: false },
   created_at: { type: Date, default: new Date() },
   updated_at: { type: Date, default: new Date() },
+  salt: { type: String, required: true },
+});
+UserSchema.method('toJSON', function () {
+  const user = this.toObject();
+  // eslint-disable-next-line no-underscore-dangle
+  delete user.__v;
+  delete user.password;
+  delete user.salt;
+  return user;
 });
 
 export default UserSchema;
