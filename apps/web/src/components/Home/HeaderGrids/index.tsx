155404b86a424354428cf345fee387de87c27b02
ragnarok <ragnarok@ragnaroks-MacBook-Pro.local>
2022-07-02 20:09:28 -0500

    [chore] fix build for server v6

--- apps/web/src/components/Home/HeaderGrids/index.tsx
+++ apps/web/src/components/Home/HeaderGrids/index.tsx
@@ -1,12 +1,11 @@
 import Image from 'next/image';
 
-// import banner1 from '../../assets/webp/banner-1.webp';
-// import banner2 from '../../assets/webp/banner-2.webp';
-// import banner3 from '../../assets/webp/banner-3.webp';
-// import banner4 from '../../assets/webp/banner-4.webp';
-// import banner5 from '../../assets/webp/banner-5.webp';
-// import banner6 from '../../assets/webp/banner-6.webp';
-import slider1 from '../../../assets/images/slider1.png';
+import banner1 from 'src/assets/images/banner-1.png';
+import banner2 from 'src/assets/images/banner-2.png';
+import banner3 from 'src/assets/images/banner-3.png';
+import banner4 from 'src/assets/images/banner-4.png';
+import banner5 from 'src/assets/images/banner-5.png';
+import banner6 from 'src/assets/images/banner-6.png';
 
 const HeaderGrids = () => (
   <div className="mx-auto mb-12 grid max-w-[1920px] grid-cols-2 gap-2 px-2.5 sm:grid-cols-9 md:mb-14 md:gap-2.5 xl:mb-16">
@@ -15,28 +14,28 @@ const HeaderGrids = () => (
         <Image
           layout="fill"
           className="w-full bg-gray-300 object-cover"
-          src={slider1}
+          src={banner1}
         />
       </a>
     </div>
     <div className="col-span-1 mx-auto sm:col-span-2">
       <a className="group relative flex h-full overflow-hidden" href="/">
-        <Image className="w-full bg-gray-300 object-cover" src={slider1} />
+        <Image className="w-full bg-gray-300 object-cover" src={banner2} />
       </a>
     </div>
     <div className="col-span-1 mx-auto sm:col-span-2">
       <a className="group relative flex h-full overflow-hidden" href="/">
-        <Image className="w-full bg-gray-300 object-cover" src={slider1} />
+        <Image className="w-full bg-gray-300 object-cover" src={banner3} />
       </a>
     </div>
     <div className="col-span-1 mx-auto sm:col-span-2">
       <a className="group relative flex h-full overflow-hidden" href="/">
-        <Image className="w-full bg-gray-300 object-cover" src={slider1} />
+        <Image className="w-full bg-gray-300 object-cover" src={banner4} />
       </a>
     </div>
     <div className="col-span-1 mx-auto sm:col-span-2">
       <a className="group relative flex h-full overflow-hidden" href="/">
-        <Image className="w-full bg-gray-300 object-cover" src={slider1} />
+        <Image className="w-full bg-gray-300 object-cover" src={banner5} />
       </a>
     </div>
     <div className="col-span-full sm:col-span-5">
@@ -44,7 +43,7 @@ const HeaderGrids = () => (
         <Image
           layout="fill"
           className="w-full bg-gray-300 object-cover"
-          src={slider1}
+          src={banner6}
         />
       </a>
     </div>
