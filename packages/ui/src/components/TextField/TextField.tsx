b4f23c4a3d49f5e472028dc7af1b1424412528e6
Emmanuel Villegas <evc.naq@hotmail.com>
2022-07-03 22:27:17 -0500

    feat[FE]: add login modal component (#15) (#17)
    * feat[FE]: add login modal component
    
    * feat[FE]: remove comment
    
    Co-authored-by: ragnarok <ragnarok@ragnaroks-MacBook-Pro.local>
    
    Co-authored-by: ragnarok <ragnarok@ragnaroks-MacBook-Pro.local>

---
+++ packages/ui/src/components/TextField/TextField.tsx
@@ -0,0 +1,27 @@
+import { ChangeEventHandler } from 'react';
+
+interface TextFieldProps {
+  value: string;
+  onChange: ChangeEventHandler<HTMLInputElement>;
+  label: string;
+}
+
+const TextField = ({ value, onChange, label }: TextFieldProps) => (
+  <div className="block">
+    <label
+      htmlFor="id"
+      className="mb-3 block cursor-pointer text-sm font-semibold leading-none text-gray-600"
+    >
+      {label}
+    </label>
+    <input
+      id="id"
+      type="text"
+      value={value}
+      onChange={onChange}
+      className="text-input font-body placeholder-body min-h-12 focus:border-heading h-11 w-full appearance-none rounded-md border border-gray-300 bg-white py-2 px-4 text-xs transition focus:outline-none md:h-12 md:px-5 lg:text-sm"
+    />
+  </div>
+);
+
+export default TextField;
