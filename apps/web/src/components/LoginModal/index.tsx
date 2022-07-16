373bff30601d97ef2e2c3c27819f97a9de89c02e
cossio0701 <47582706+cossio0701@users.noreply.github.com>
2022-07-15 21:51:44 -0500

    Feat/add login form in modal (#23)
    * feat [FE] add login form in modal
    
    * feat [FE] add facebook and google icon to the buttons
    
    Co-authored-by: Kevin Cossio <kevinc@companycreativa.com>

--- apps/web/src/components/LoginModal/index.tsx
+++ apps/web/src/components/LoginModal/index.tsx
@@ -1,6 +1,10 @@
-import { Dialog, Transition } from '@headlessui/react';
+import { Dialog, Transition, Switch } from '@headlessui/react';
 import { Fragment, useState } from 'react';
-import { Button } from 'ui';
+import { Button, TextField } from 'ui';
+
+import Logo from 'src/assets/svgs/logo.svg';
+import FacebookIconSquared from 'src/assets/svgs/FacebookIconSquared.svg';
+import GoogleIconSquared from 'src/assets/svgs/GoogleIconSquared.svg';
 
 const MyModal = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -48,27 +52,78 @@ const MyModal = () => {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                  <Dialog.Title
-                    as="h3"
-                    className="text-lg font-medium leading-6 text-gray-900"
-                  >
-                    Payment successful
-                  </Dialog.Title>
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-500">
-                      Your payment has been successfully submitted. We’ve sent
-                      you an email with all of the details of your order.
-                    </p>
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-8 text-left align-middle shadow-xl transition-all">
+                  <div className="mb-6">
+                    <Dialog.Title className="flex flex-col items-center">
+                      <Logo />
+                    </Dialog.Title>
+                    <Dialog.Description className="mt-3 flex flex-col items-center text-gray-500">
+                      Login with your email & password
+                    </Dialog.Description>
                   </div>
-
-                  <div className="mt-4">
+                  <form>
+                    <TextField label="Email" value="" onChange={() => {}} />
+                    <TextField label="Password" value="" onChange={() => {}} />
+                    <div className="mt-3 flex justify-between">
+                      <div>
+                        <Switch.Group>
+                          <Switch
+                            checked={false}
+                            onChange={() => {}}
+                            className={`${
+                              false ? 'bg-blue-600' : 'bg-gray-200'
+                            } relative inline-flex h-6 w-11 items-center rounded-full`}
+                          >
+                            <span className="sr-only text-sm ">
+                              Enable notifications
+                            </span>
+                            <span
+                              className={`${
+                                false ? 'translate-x-6' : 'translate-x-1'
+                              } inline-block h-4 w-4 transform rounded-full bg-white`}
+                            />
+                          </Switch>
+                          <Switch.Label className="mr-4 ml-2">
+                            Enable notifications
+                          </Switch.Label>
+                        </Switch.Group>
+                      </div>
+                      <button type="button" className="text-sm underline">
+                        Forgot password?
+                      </button>
+                    </div>
                     <button
                       type="button"
-                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                      onClick={closeModal}
+                      className="mt-5 w-full rounded-md bg-neutral-800 py-3 px-8 text-sm text-white"
                     >
-                      Got it, thanks!
+                      Login
+                    </button>
+                  </form>
+                  <div className="relative mt-6 mb-3.5 flex flex-col items-center justify-center text-sm ">
+                    <hr className="w-full border-gray-300" />
+                    <span className="absolute -top-2.5 bg-white px-2">Or</span>
+                  </div>
+                  <button
+                    type="button"
+                    className="bg-facebok mt-4 flex w-full items-center justify-center gap-2 rounded-md py-3 px-8 text-sm text-white"
+                  >
+                    <FacebookIconSquared className="text-base" />
+                    <span>Login With Facebook</span>
+                  </button>
+                  <button
+                    type="button"
+                    className="bg-google mt-2 flex w-full items-center justify-center gap-2 rounded-md py-3 px-8 text-sm text-white"
+                  >
+                    <GoogleIconSquared className="text-base" />
+                    <span>Login With Google</span>
+                  </button>
+                  <div className="mt-6 text-center text-sm text-gray-500 ">
+                    Dont have any account?
+                    <button
+                      type="button"
+                      className="ml-1 text-sm font-bold text-black underline"
+                    >
+                      Register
                     </button>
                   </div>
                 </Dialog.Panel>
--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     '../../packages/ui/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        facebok: '#4267B2',
+        google: '#4285F4',
+      },
+    },
   },
   plugins: [],
 };
