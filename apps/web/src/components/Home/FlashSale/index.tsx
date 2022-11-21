8241fac5d569ad1a4ad91a9159fb24798259703e
SgutierrezG <segut10@gmail.com>
2022-11-21 15:43:39 -0500

    add responsive section flash sale

--- apps/web/src/components/Home/FlashSale/index.tsx
+++ apps/web/src/components/Home/FlashSale/index.tsx
@@ -36,7 +36,7 @@ const FlashSale = () => (
                 </div>
             </div>
 
-            <div className='grid grid-cols-5 grid-rows-2 gap-4'>
+            <div className='grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-x-3 md:gap-x-5 xl:gap-x-7 gap-y-4 lg:gap-y-5 xl:gap-y-6 2xl:gap-y-8'>
 
                 <FlashSaleComponent
                     labelTitle='Adidas Shoes Black'
